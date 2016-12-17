7f194dd747c9de0e906255b28221ecc60b0837f7
Luis Herranz Mañas <luishmanas@gmail.com>
2016-12-17 13:40:45 +0000

    Inverting the joystick on camera change

---
+++ Assets/KoskiMiniAssets/Scripts/JoystickCameraController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using System;
+
+public class JoystickCameraController : MonoBehaviour {
+
+    JoyStickCustomController joyStickController;
+
+
+    // Use this for initialization
+    void Start () {
+
+        // Get the joystick
+        GameObject joyObject = GameObject.Find("MobileJoystick");
+        try
+        {
+            joyStickController = (JoyStickCustomController)joyObject.GetComponent<JoyStickCustomController>();
+            joyStickController.configureJoystick(false, true, true);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e, joyObject);
+        }
+
+    }
+	
+
+    private void DetermineZone()
+    {
+        if(0 <= transform.rotation.y && transform.rotation.y < 90)
+        {
+            joyStickController.configureJoystick(true, false, true);
+        }
+        else if(90 <= transform.rotation.y && transform.rotation.y < 180)
+        {
+            joyStickController.configureJoystick(true, true, true);
+        }
+        else if ((180 <= transform.rotation.y && transform.rotation.y < 270) || (transform.rotation.y < 0 && transform.rotation.y>-90) )
+        {
+            joyStickController.configureJoystick(false, true, true);
+        }
+        else if ((270 <= transform.rotation.y && transform.rotation.y < 360) || (transform.rotation.y < -90 && transform.rotation.y > -180))
+        {
+            joyStickController.configureJoystick(false, false, true);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+}
