9e6a4e082732df4590e84c788de3a37e8d557b8b
Luis Herranz Mañas <luishmanas@gmail.com>
2017-01-10 11:08:30 +0000

    Playing with Lighting
    - Changes and experiments to lighting in order to fix shading problems.
    - Initialized the sound system

---
+++ Assets/KoskiMiniAssets/Scripts/SoundController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundController : MonoBehaviour {
+
+
+
+    AudioSource audioS;
+    public AudioClip SoundCoinCollected, SoundPlayerHit, SoundPlayerDeath, Sound1Up;
+    public AudioClip SoundGameWon, SoundGameLost;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
