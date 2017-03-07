76e04d3e88d4e845e8ea40cc047493412aa7a394
Luis Herranz Mañas <luishmanas@gmail.com>
2017-03-07 11:34:26 +0000

    Added Growing element´s script
    Animations need to be initialized.

---
+++ Assets/KoskiMiniAssets/Scripts/GrowingElement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+
+
+public class GrowingElement : MonoBehaviour {
+
+    public float distanceTH = 5;
+    protected float distance2Player;
+    protected Transform thePlayer;
+    protected Animator animationElements;
+    protected bool growthTriggered = false;
+
+    // Use this for initialization
+    protected void Start() {
+        thePlayer = GameObject.Find("Player").transform;
+        animationElements = this.GetComponent<Animator>();
+    }
+	
+    protected void checkDistance()
+    {
+        distance2Player = (thePlayer.position - this.GetComponent<Transform>().position).magnitude;
+        if (distance2Player <= distanceTH)
+        {
+            growthTriggered = true;
+        }
+    }
+
+
+
+    // Update is called once per frame
+    protected void Update () {
+
+        
+
+        if (growthTriggered)
+        {
+            animationElements.SetBool("DoGrowth", growthTriggered);
+        }
+        else
+        {
+            checkDistance();
+        }        
+
+
+
+    }
+}
---
+++ Assets/KoskiMiniAssets/Scripts/GrowingElementTooglable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowingElementTooglable : GrowingElement
+{
+
+	
+	// Update is called once per frame
+	new void Update () {
+
+        checkDistance();
+        animationElements.SetBool("DoGrowth", growthTriggered);
+    }
+
+
+}
