49acdf58c67fa7115cdd796017b14ac881a584d0
Unknown <aurielle.philippart@gmail.com>
2017-03-21 19:37:43 +0100

    River move and rocks
    News scripts for wavy effect + cloth effect

---
+++ Assets/WaveController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour {
+
+    public float height;
+    public float time; //duration of animation cycle
+
+    void Start()
+    {
+        iTween.MoveBy(this.gameObject, iTween.Hash("y", height, "time", time, "looptype", "pingpong", "easetype", iTween.EaseType.easeInOutSine));
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
