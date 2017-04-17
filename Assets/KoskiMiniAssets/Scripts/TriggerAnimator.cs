4334cdab1174080a632cb6bd518a73baae6a4b01
Luis Herranz Mañas <luishmanas@gmail.com>
2017-04-17 17:03:43 +0100

    Created Script to animate mushrooms
    Now they will appear from the ground and dance when idle.

---
+++ Assets/KoskiMiniAssets/Scripts/TriggerAnimator.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+
+
+public class TriggerAnimator : MonoBehaviour {
+
+    public float distanceTH;
+    public float distance2Player;
+    protected Transform thePlayer;
+    protected Animator animationElements;
+    protected bool growthTriggered = false;
+    protected bool doOneTime = false;
+    public int numAnimatorBranches = 2;
+
+    // Use this for initialization
+    protected void Start()
+    {
+        thePlayer = GameObject.Find("Player").transform;
+        animationElements = this.GetComponent<Animator>();
+    }
+
+
+
+    /// <summary>
+    /// Checks if the distance between the player and the object is sufficiently small.
+    /// </summary>
+    protected void checkDistance()
+    {
+        distance2Player = (thePlayer.position - this.GetComponent<Transform>().position).magnitude;
+        if (distance2Player <= distanceTH)
+        {
+            growthTriggered = true;
+        }
+    }
+
+
+
+    // Update is called once per frame
+    protected void Update()
+    {
+
+
+
+        if (growthTriggered && !doOneTime)
+        {
+            animationElements.SetBool("DoGrowth", growthTriggered);
+            int selected = (int)Random.Range(0, numAnimatorBranches);
+            //Debug.LogError("Triggered: " + selected);
+            animationElements.SetInteger("AnimVersion", selected);
+            /*
+            if (transform.FindChild("Particles").GetComponent<ParticleSystem>())
+            {
+                // Debug.LogError("Starting particles");
+                transform.FindChild("Particles").GetComponent<ParticleSystem>().enableEmission = true;
+                transform.FindChild("Particles").GetComponent<ParticleSystem>().Play();
+            }
+            */
+            doOneTime = true;
+        }
+        else if (!doOneTime)
+        {
+            checkDistance();
+        }
+
+
+
+    }
+}
