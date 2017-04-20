5d56ee37b708eb2aa76b3e3259eb3acca6007ff3
Unknown <aurielle.philippart@gmail.com>
2017-04-20 17:21:24 +0200

    Particles effects formation
    testes that might be useful for the future

---
+++ Assets/Particles/Udemy_courses/Object_fall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_fall : MonoBehaviour {
+
+    public GameObject groundpound;
+    public GameObject explosion;
+    public GameObject explosionDust;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Instantiate(explosion, transform.position, explosion.transform.rotation);
+            Instantiate(explosionDust, transform.position, explosionDust.transform.rotation);
+        }
+	}
+    void OnCollisionEnter (Collision col)
+    {
+        Instantiate(groundpound, transform.position, groundpound.transform.rotation);
+    }
+}
